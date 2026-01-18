408c9bf83935e1b78d0c0e5ee0fe7f958e8e9199
Saifurrahman <saifur13233@gmail.com>
2026-01-18 13:08:44 +0530

    Added user authentication and authorization for listings and reviews with login protection

--- init/index.js
+++ init/index.js
@@ -4,20 +4,24 @@ const Listing = require("../models/listing.js");
 
 const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
 main()
-  .then(()=> {
+  .then(() => {
     console.log("Coneected to Database");
   })
   .catch((err) => {
     console.log(err);
-  })
-async function main(){
+  });
+async function main() {
   await mongoose.connect(MONGO_URL);
 }
 
-const initDB = async() => {
+const initDB = async () => {
   await Listing.deleteMany({});
+  initData.data = initData.data.map((obj) => ({
+    ...obj,
+    owner: "696a8e3095bd175399163ff5",
+  }));
   await Listing.insertMany(initData.data);
-  console.log("Data was initialized"); 
+  console.log("Data was initialized");
 };
 
 initDB();
